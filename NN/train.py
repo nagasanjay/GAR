461771c4283b3947fff18a593eed222f80acf09d
San <43279026+nagasanjay@users.noreply.github.com>
2020-10-08 16:48:33 +0530

    completed train.py

--- NN/train.py
+++ NN/train.py
@@ -1,11 +1,16 @@
-#import tensorflow as tf
-#import matplotlib.pyplot as plt
+import tensorflow as tf
+import matplotlib.pyplot as plt
 import argparse
 from helper import load_images, load_signals, norm_img
 from nn import generate
+from sklearn.model_selection import train_test_split
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from config import EPOCHS, BATCH_SIZE
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--path", help="dataset path", required=True)
+parser.add_argument("-p", "--path", help="dataset path", default="dataset")
 args = parser.parse_args()
 
 DATASET_BASE_PATH = args.path
@@ -16,4 +21,10 @@ images = load_images(DATASET_IMAGES, DATASET_SIGNALS)
 images = norm_img(images)
 signals = load_signals(DATASET_SIGNALS)
 
+images_train, images_test, signals_train, signals_test = train_test_split(
+            images, signals, test_size=0.33)
+
 model = generate(shape=images[0].shape, flatten="conv1d")
+model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+history = model.fit(images_train, signals_train, epochs=EPOCHS,
+            validation_data=(images_test, signals_test))
--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 BASE_PATH += "\\"
+
+EPOCHS = 20
+BATCH_SIZE = 5
