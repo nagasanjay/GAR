b422984518a1c3265967b3d68a2f2abc763b9c76
San <43279026+nagasanjay@users.noreply.github.com>
2020-10-08 21:55:23 +0530

    completed training part

--- NN/helper.py
+++ NN/helper.py
@@ -8,25 +8,42 @@ from PIL import Image
 
 def load_signals(filename):
     signals = []
+    
+    if os.path.exists(BASE_PATH + filename + ".npz"):
+        signals = np.load(BASE_PATH + filename + ".npz", allow_pickle=True)
+        return np.array(signals['signals'])
+
     with open(BASE_PATH + filename) as file:
         reader = csv.reader(file, delimiter=',')
         for row in reader:
-            signals.append(row[1:4])
+            signals.append([row[1:4]])
+    print(signals)
     signals = np.array(signals, dtype=np.float32)
+    print(signals)
+    np.savez_compressed(BASE_PATH + filename, signals=signals)
     return signals
+
+def open_and_reshape(fname):
+    img = Image.open(fname).resize((144, 144))
+    return img
+    
 
 def load_images(path, filename):
     images = []
+    files = []
+
+    if os.path.exists(BASE_PATH + path + "img.npz"):
+        images = np.load(BASE_PATH + path + "img.npz", allow_pickle=True)
+        return np.array(images['images'])
+
     with open(BASE_PATH + filename) as file:
         reader = csv.reader(file, delimiter=',')
         for row in reader:
-            img = Image.open(BASE_PATH + path + row[0])
-            img = np.array(img)
-            images.append(img)
-    images = np.array(images)
+            files.append(row[0])
+        images = np.array([np.array(open_and_reshape(BASE_PATH + path + fname)) for fname in files])
+    np.savez_compressed(BASE_PATH  + path + "img", images=images)
     return images
 
 def norm_img(img):
     img = img / 255.0
     return img
-
--- NN/train.py
+++ NN/train.py
@@ -21,19 +21,45 @@ images = load_images(DATASET_IMAGES, DATASET_SIGNALS)
 images = norm_img(images)
 signals = load_signals(DATASET_SIGNALS)
 
-images_train, images_test, signals_train, signals_test = train_test_split(
-            images, signals, test_size=0.33)
+images_train, images_test, signals_train, signals_test = train_test_split(images, signals, test_size=0.33)
+
+train_dataset = tf.data.Dataset.from_tensor_slices((images_train, signals_train))
+train_dataset = train_dataset.batch(BATCH_SIZE)
+test_dataset = tf.data.Dataset.from_tensor_slices((images_test, signals_test))
+test_dataset = test_dataset.batch(BATCH_SIZE)
 
 model = generate(shape=images[0].shape, flatten="conv1d")
 model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-history = model.fit(images_train, signals_train, epochs=EPOCHS, batch_size=BATCH_SIZE
-            validation_data=(images_test, signals_test))
 
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
+checkpoint_path = "NN/checkpoint/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True,
+                                                 verbose=1, save_freq = 2)
+
+history = model.fit(train_dataset, epochs=EPOCHS,
+            validation_data=test_dataset, callbacks=[cp_callback])
+
+print(model.metrics_names)
+print(history.history.keys())
+plt.figure(figsize=(15, 4))
+acc = history.history['dense_1_accuracy']
+val_acc = history.history['val_dense_1_accuracy']
+
+loss=history.history['dense_1_loss']
+val_loss=history.history['val_dense_1_loss']
+
+epochs_range = range(EPOCHS)
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
 plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
 
-test_loss, test_acc = model.evaluate(images_test,  signals_test, verbose=2)
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+model.save("model")
