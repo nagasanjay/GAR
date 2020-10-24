24dc78c92a64550f68647d668fc12a917df7f4ba
san <nagasanjayvijayan@gmail.com>
2020-10-24 21:31:57 +0530

    added speed as input to NN, squeezed conv1D flat to concatenate

--- NN/model.py
+++ NN/model.py
@@ -1,6 +1,7 @@
 # necessary imports
+import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, Conv1D, MaxPooling2D
+from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, Conv1D, MaxPooling2D, concatenate
 from tensorflow.keras.utils import plot_model
 import sys
 
@@ -16,33 +17,40 @@ import sys
 #
 # example:      generate((144, 144, 3), 'conv1d')
 
-def generate(shape=(144, 144, 3), option="flatten"):
+def generate(shape=(144, 144, 1), option="flatten"):
     visible = Input(shape=shape)
+    speed = Input(shape=(1, ))
 
-    conv1 = Conv2D(64, kernel_size=5, activation='relu', padding='same')(visible)
+    conv1 = Conv2D(16, kernel_size=5, activation='relu', padding='same')(visible)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
     conv2 = Conv2D(32, kernel_size=3, activation='relu', padding='same')(pool1)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    conv3 = Conv2D(16, kernel_size=3, activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(64, kernel_size=3, activation='relu', padding='same')(pool2)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(128, kernel_size=3, activation='relu', padding='same')(pool3)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
     # way 1 to flatten - many params
     if option == 'flatten':
-        flat = Flatten()(pool3)
+        flat = Flatten()(pool4)
 
     # way 2 to flatten - less params
     elif option == 'conv1d':
-        conv4 = Conv1D(128, kernel_size=3, activation='relu', padding='same')(pool3)
-        flat  = MaxPooling2D(pool_size=(conv4.shape[1], conv4.shape[2]))(conv4)
+        conv5 = Conv1D(128, kernel_size=3, activation='relu', padding='same')(pool4)
+        flat  = MaxPooling2D(pool_size=(conv5.shape[1], conv5.shape[2]))(conv5)
+        flat = tf.squeeze(flat, axis=[1, 2])
 
     # mentioned method doesn't exist
     else:
         print("\nInvalid flatten option")
         sys.exit(1)
 
-    hidden1 = Dense(32, activation='relu')(flat)
+    concat = concatenate([speed, flat])
+
+    hidden1 = Dense(32, activation='relu')(concat)
 
     # throttle
     output1 = Dense(1, activation='linear')(hidden1)
@@ -51,9 +59,9 @@ def generate(shape=(144, 144, 3), option="flatten"):
     # steering
     output3 = Dense(1, activation='linear')(hidden1)
 
-    model = Model(inputs=visible, outputs=[output1, output2, output3])
+    model = Model(inputs=[visible, speed], outputs=[output1, output2, output3])
     return model
 
-model = generate()
+model = generate(option='conv1d')
 model.summary()
 plot_model(model, show_shapes=True)
